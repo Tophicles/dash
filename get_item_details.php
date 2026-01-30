bdf64ddc7304bcb1366b10a781e80c46dc4aa3e1
Tophicles <tophicles@gmail.com>
2026-01-29 21:12:46 -0500

    Revert "Refactor item modal: moved tech info to badges under metadata and sty…"

--- get_item_details.php
+++ get_item_details.php
@@ -212,12 +212,7 @@ try {
                 } elseif (($stream['Type'] ?? '') === 'Audio') {
                      if (empty($item['audioCodec']) || ($stream['IsDefault'] ?? false)) {
                         $item['audioCodec'] = $stream['Codec'] ?? '';
-
-                        $channels = $stream['Channels'] ?? '';
-                        if ($channels == '2') $channels = '2.0';
-                        elseif ($channels == '6') $channels = '5.1';
-                        elseif ($channels == '8') $channels = '7.1';
-                        $item['audioChannels'] = $channels;
+                        $item['audioChannels'] = $stream['Channels'] ?? '';
                      }
                 }
             }
@@ -306,14 +301,7 @@ try {
             $media = $metadata['Media'][0];
             $item['videoCodec'] = $media['videoCodec'] ?? '';
             $item['audioCodec'] = $media['audioCodec'] ?? '';
-
-            // Map common channel counts to friendly names
-            $channels = $media['audioChannels'] ?? '';
-            if ($channels == '2') $channels = '2.0';
-            elseif ($channels == '6') $channels = '5.1';
-            elseif ($channels == '8') $channels = '7.1';
-
-            $item['audioChannels'] = $channels;
+            $item['audioChannels'] = $media['audioChannels'] ?? '';
             $item['resolution'] = $media['videoResolution'] ?? '';
             $item['container'] = $media['container'] ?? '';
 
